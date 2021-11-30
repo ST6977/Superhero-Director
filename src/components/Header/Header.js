941988977391da95011bc6e2a6c1735fc1df415b
ST6977 <sumaiyat610@gmail.com>
2021-11-30 03:34:10 -0800

    now added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 // Header Component
 const Header = () => {
-    return (
-        <div className="header">
-            <h2 className="main-title">Make a Team For Your IT Business</h2>
-            <p className="header-des">The Modern Technology Make Our Life Very Easy.Developers Makes It Easy For Us!!</p>
-            <h2 className="total-expense">Total Expense: <span className="balance">500 Million</span></h2>
-        </div>
-    );
+  return (
+    <div className="header">
+      <h2 className="main-title">Make a Team For Your IT Business</h2>
+      <p className="header-des">
+        The Modern Technology Make Our Life Very Easy.Developers Makes It Easy
+        For Us!!
+      </p>
+      <h2 className="total-expense">
+        Total Expense: <span className="balance">700 Million</span>
+      </h2>
+    </div>
+  );
 };
 
 export default Header;
